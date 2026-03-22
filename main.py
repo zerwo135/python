9d904f4b2c7d450af347007439d7a6d5737a473e
zerwo135 <samdihaev206@gmail.com>
2026-03-22 23:35:36 +0300

    Курс до пункта 2.4

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+import pandas as pd
+
+df = pd.read_csv('/Users/makbuk/VSCode/Python/students.csv', delimiter=',')
+kfcLovers = df[df['Fastfood']=='KFC']['Weight']
+bkLovers = df[df['Fastfood']=='Бургер кинг']['Weight']
+mcdLovers = df[df['Fastfood']=='Макдональдс (или как он там сейчас называется?)']['Weight']
+meanKfcWeigth = kfcLovers.mean()
+meanBkWeigth = bkLovers.mean()
+meanMcdWeigth = mcdLovers.mean()
+print(meanKfcWeigth-meanMcdWeigth)
